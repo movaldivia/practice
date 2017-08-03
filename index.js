5b2dd0b475e155984ac623b5489195b80f1583eb
negebauer <negebauer@uc.cl>
2017-08-03 16:30:19 -0400

    Basic routes

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const Koa = require('koa')
+const Router = require('koa-router')
 
 const app = new Koa()
+const router = new Router()
 
-app.use(ctx => {
+router.use(async (ctx, next) => {
+  console.log('Headers', ctx.headers)
+  console.log('Params', ctx.params)
+  await next()
+})
+
+router.get('/', ctx => {
   ctx.body = {
     message: "Hello world"
   }
 })
+
+router.post('/', ctx => {
+  ctx.body = {
+    message: "What u doing?"
+  }
+})
+
+app.use(router.routes())
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, err => {
