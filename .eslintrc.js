f739947d2c94f65d2263f8c59931cf86fe1e9351
Nicolás Gebauer <negebauer@uc.cl>
2017-08-03 15:59:25 -0400

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaVersion: 2017,
+    sourceType: "module",
+  },
+  env: {
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "prettier",
+  ],
+  plugins: [
+    "prettier",
+  ],
+  rules: {
+    "prettier/prettier": ["error", {
+      trailingComma: "es5",
+      semi: false,
+    }],
+  },
+};
